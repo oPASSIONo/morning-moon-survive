f7a1e7c6902e4f178eb162545b5a7e5090e8340c
panchanok1508 <panchanok.piro@bumail.net>
2024-04-02 11:10:34 +0700

    Add Inventory Input
    Add Open Inventory in Input system
    - still working on Inventory Managing (UI)

---
+++ morning-moon-survive/Assets/Scripts/UI/InventoryController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InventoryController : MonoBehaviour
+{
+    [SerializeField] private UIInventoryPage inventoryUI;
+
+    public int inventorySize = 10;
+    
+    private PlayerInput playerInput;
+    private InputAction openInventoryAction;
+    
+    
+    void Start()
+    {
+        inventoryUI.InitializeInventoryUI(inventorySize);
+        playerInput = GetComponent<PlayerInput>();
+        openInventoryAction = playerInput.actions.FindAction("Inventory");
+    }
+
+    void Update()
+    {
+        if (openInventoryAction.triggered)
+        {
+            OpenInventoryUI();
+        }
+    }
+
+    void OpenInventoryUI()
+    {
+        inventoryUI.Show();
+    }
+
+}
