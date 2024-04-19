926aad0193ef5a19dedc0b00e1c865f687c28dab
oPASSIONo <pupak.atch@bumail.net>
2024-04-19 19:44:21 +0700

    fix animation Rpc

--- morning-moon-survive/Assets/Scripts/Animation/AnimationStateController.cs
+++ morning-moon-survive/Assets/Scripts/Animation/AnimationStateController.cs
@@ -1,19 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class AnimationStateController : MonoBehaviour
+public class AnimationStateController : NetworkBehaviour
 {
-    public static AnimationStateController Instance { get; private set; }
+     public static AnimationStateController Instance { get; private set; }
         
     private Animator animator;
     private int isWalkingHash;
     private int isTriedHash;
 
-    
-    //public Hunger Hunger { private get; set; }
     [SerializeField] private Hunger Hunger;
+
     private void Start()
     {
         animator = GetComponent<Animator>();
@@ -22,20 +22,39 @@ public class AnimationStateController : MonoBehaviour
         isTriedHash = Animator.StringToHash("isTried");
     }
     
-
     private void Update()
     {
-        WalkingState(Hunger.CurrentHunger);
-    }
+        if (!IsOwner) return;
 
-    private void WalkingState(int currentHunger)
-    {
-        bool isWalking = animator.GetBool(isWalkingHash);
-        bool isTried = animator.GetBool(isTriedHash);
+        // Detect player input
         bool forwardPressed = Input.GetKey(KeyCode.W);
         bool leftPressed = Input.GetKey(KeyCode.A);
         bool backwardPressed = Input.GetKey(KeyCode.S);
         bool rightPressed = Input.GetKey(KeyCode.D);
+
+        // Call the server RPC to update walking state
+        UpdateWalkingStateServerRpc(Hunger.CurrentHunger, forwardPressed, leftPressed, backwardPressed, rightPressed);
+    }
+
+
+    [ServerRpc]
+    private void UpdateWalkingStateServerRpc(int currentHunger, bool forwardPressed, bool leftPressed, bool backwardPressed, bool rightPressed)
+    {
+        UpdateWalkingState(Hunger.CurrentHunger, forwardPressed, leftPressed, backwardPressed, rightPressed);
+        UpdateWalkingStateClientRpc(currentHunger, forwardPressed, leftPressed, backwardPressed, rightPressed);
+    }
+
+    [ClientRpc]
+    private void UpdateWalkingStateClientRpc(int currentHunger, bool forwardPressed, bool leftPressed, bool backwardPressed, bool rightPressed)
+    {
+        if (!IsOwner) return;
+        UpdateWalkingState(currentHunger, forwardPressed, leftPressed, backwardPressed, rightPressed);
+    }
+
+    private void UpdateWalkingState(int currentHunger, bool forwardPressed, bool leftPressed, bool backwardPressed, bool rightPressed)
+    {
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool isTried = animator.GetBool(isTriedHash);
     
         // Check if any movement key is pressed
         bool anyMovementKeyPressed = forwardPressed || leftPressed || backwardPressed || rightPressed;
@@ -45,6 +64,7 @@ public class AnimationStateController : MonoBehaviour
             if (!isTried && anyMovementKeyPressed)
             {
                 animator.SetBool(isTriedHash, true);
+                animator.SetBool(isWalkingHash, false);
             }
     
             if (isTried && !anyMovementKeyPressed)
@@ -64,6 +84,6 @@ public class AnimationStateController : MonoBehaviour
                 animator.SetBool(isWalkingHash, false);
             }
         }
-       
     }
+   
 }
