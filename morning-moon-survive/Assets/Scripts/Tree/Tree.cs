e7051e7dc6d7e7b4b4b0b6219c0df1bee14150fd
panchanok1508 <panchanok.piro@bumail.net>
2024-07-12 13:03:23 +0700

    Change Tree to Log when Health is Halve
    - Can't Interact to pickup the wood log item

--- morning-moon-survive/Assets/Scripts/Tree/Tree.cs
+++ morning-moon-survive/Assets/Scripts/Tree/Tree.cs
@@ -2,22 +2,31 @@ using UnityEngine;
 
 public class Tree : MonoBehaviour
 {
-    [SerializeField] private float health = 100f;
+    [SerializeField] private float initialHealth = 100f; // Store the initial health value
+    private float health;
     public GameObject[] dropItems; // Array of items to drop when the tree falls
+    [SerializeField] private GameObject[] leaf;
 
+    
+    private void Start()
+    {
+        health = initialHealth; // Initialize health to the initial value
+    }
+    
     public void TakeDamage(float amount)
     {
         health -= amount;
         Debug.Log($"Tree took {amount} damage. Remaining health: {health}");
-
-        if (health <= 0)
+        
+        CheckState();
+        /*if (health <= 0)
         {
             DropItem();
             Destroy(gameObject);
-        }
+        }*/
     }
 
-    public void DropItem()
+    private void DropItem()
     {
         if (dropItems != null && dropItems.Length > 0)
         {
@@ -27,6 +36,29 @@ public class Tree : MonoBehaviour
                 {
                     Instantiate(item, transform.position, Quaternion.identity);
                 }
+            }
+        }
+    }
+
+    private void CheckState()
+    {
+        if (health <= (initialHealth / 2)) // Compare with half of the initial health
+        {
+            SetLeavesActive(false);
+        }
+        if (health <= 0)
+        {
+            DropItem();
+            Destroy(gameObject);
+        }
+    }
+    private void SetLeavesActive(bool isActive)
+    {
+        foreach (GameObject leafObject in leaf)
+        {
+            if (leafObject != null)
+            {
+                leafObject.SetActive(isActive);
             }
         }
     }
