f0db43f041f31749c77f14a67d01ac272ed79040
Dolphin48 <104317029+Dolphin48@users.noreply.github.com>
2024-08-13 03:54:19 +0700

    Replace Sprite & Update Health bar

--- morning-moon-survive/Assets/ParticleEffect/EnemyHealthBar.cs
+++ morning-moon-survive/Assets/ParticleEffect/EnemyHealthBar.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,6 +6,7 @@ public class EnemyHealthBar : MonoBehaviour
 {
     public Health healthComponent;
     public Slider slider;
+    public TMP_Text enemyHealthText;
 
     void Start()
     {
@@ -12,10 +14,14 @@ public class EnemyHealthBar : MonoBehaviour
         
         slider.maxValue = healthComponent.MaxHealth;
         slider.value = healthComponent.CurrentHealth;
+        
+        enemyHealthText.text = $"{healthComponent.CurrentHealth}/{healthComponent.MaxHealth}";
     }
 
     public void UpEnemydateHealthBar(float currentHealth, float maxHealth, float minHealth)
     {
         slider.value = currentHealth;
+        
+        enemyHealthText.text = $"{currentHealth}/{maxHealth}";
     }
 }
