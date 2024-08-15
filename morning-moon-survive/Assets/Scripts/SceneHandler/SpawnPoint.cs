145ccc3effcfb6533fce1f81f7f2c57daa7537f9
panchanok1508 <panchanok.piro@bumail.net>
2024-08-15 12:55:57 +0700

    Fix Movetarget In GameManager

--- morning-moon-survive/Assets/Scripts/SceneHandler/SpawnPoint.cs
+++ morning-moon-survive/Assets/Scripts/SceneHandler/SpawnPoint.cs
@@ -1,16 +1,14 @@
 using UnityEngine;
 
-
 public class SpawnPoint : MonoBehaviour
 {
-    [SerializeField] private string spawnPointTag;
+    [SerializeField] private string spawnPointName;
 
-    private void Awake()
+    private void Start()
     {
         if (SpawnPointManager.Instance != null)
         {
-            SpawnPointManager.Instance.RegisterSpawnPoint(spawnPointTag, gameObject);
+            SpawnPointManager.Instance.RegisterSpawnPoint(spawnPointName, transform);
         }
-        
     }
 }
